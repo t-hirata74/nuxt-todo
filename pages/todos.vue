1c6dd841c4cd7116bbbd3e98788bfd72525fac7b
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-20 14:02:06 +0900

    add strikethrough

--- pages/todos.vue
+++ pages/todos.vue
@@ -5,8 +5,9 @@
       <li v-for="todo in todos" :key="todo.id">
         <!-- {{ todo }} -->
         <input type="checkbox" v-bind:checked="todo.done" @change="toggle(todo)">
-        
-        {{ todo.name }} {{ todo.created }}
+        <span v-bind:class="{done: todo.done}">
+          {{ todo.name }} {{ todo.created }}
+        </span>
         <button v-on:click="remove(todo.id)" >X</button>
       </li>
     </ul>
@@ -48,3 +49,9 @@
     }
   }
 </script>
+
+<style>
+li > span.done {
+  text-decoration: line-through;
+}
+</style>
